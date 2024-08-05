ffac53684546b3f4a2c7166280dadda8860a6e06
sandwidiabdoulrafi <abdoulrafisandwidi@gmail.com>
2024-08-05 10:55:18 +0200

    End initialization payment page

---
+++ serveur.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { get } = require('http');
+const app = express();
+const path = require('path');
+
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/submit',(req, res) =>{
+    const getData = req.body;
+    res.send('donnée recu');
+});
+app.listen(8808, ()=>{
+    console.log('le serveur fonctionne avec le port 8808');
+})
